2775529f1764ae7480b5a492a28fc7b645a18342
Tim Mathews <timm@compusult.net>
2024-03-04 17:06:50 -0330

    CHORE: Break up Docker images
    Change-Id: I1cf6a11de702ebaaf5fd41a5601e20cdc09b2c1b

--- backend/run.sh
+++ backend/run.sh
@@ -2,6 +2,4 @@
 script=$(readlink -f "$0")
 dirname=$(dirname "$script")
 rootdir=$(dirname "$dirname")
-env_path="$rootdir/config/env"
-export $(cat "$env_path" | xargs)
 nohup "$dirname"/cslt3d > "$rootdir"/logs/backend.log 2>&1 &
--- config/webpack.master.config.js
+++ config/webpack.master.config.js
@@ -2,6 +2,7 @@ const TARGET = process.env.npm_lifecycle_event;
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common.config.js");
 switch (TARGET) {
+    case "build-client":
     case "full-build": {
         module.exports = merge(commonConfig, {
             mode: "development",
@@ -9,24 +10,8 @@ switch (TARGET) {
         });
         break;
     }
-    case "build-client": {
-        module.exports = merge(commonConfig, {
-            mode: "development",
-            devtool: "source-map"
-        });
-        break;
-    }
-    case "full-build-release": {
-        module.exports = merge(commonConfig, {
-            mode: "production",
-            performance: {
-                hints: false,
-                maxEntrypointSize: 5120000,
-                maxAssetSize: 5120000
-            }
-        });
-        break;
-    }
+    case "docker-frontend":
+    case "full-build-release":
     case "build-client-release": {
         module.exports = merge(commonConfig, {
             mode: "production",
