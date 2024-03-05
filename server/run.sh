e99cbb5d86d9ae8b2770d40fb8825f170212362d
Josh Hussey <josh@compusult.net>
2024-03-05 13:58:17 +0100

    CHORE: Break everything to do it again a different way for no reason
    Successfully broke everything.
    
    Change-Id: If39d105a3ddbe5ab099950a751d31d527fd380cb

--- server/run.sh
+++ server/run.sh
@@ -3,5 +3,4 @@ script=$(readlink -f "$0")
 dirname=$(dirname "$script")
 rootdir=$(dirname "$dirname")
 env_path="$rootdir/config/env"
-export $(cat "$env_path" | xargs)
 nohup "$dirname"/caddy run --config "$dirname"/caddyfile --adapter caddyfile > "$rootdir"/logs/run.log 2>&1
