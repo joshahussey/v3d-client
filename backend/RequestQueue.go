ef225a9afd17e88da38f4bcc2ef4c05f8ff8da05
Josh Clarke <joshua@compusult.net>
2024-05-24 14:46:48 -0230

    99999-2174 - 3D Standalone WES Integration
    Refactor the request queue to correctly send requests in order.
    
    Change-Id: Iba7997f8ded449b313eb26209337bca93b4f363f

--- backend/RequestQueue.go
+++ backend/RequestQueue.go
@@ -2,90 +2,143 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
-    "strings"
 )
 
+type RequestQueue struct {
+	requestSessionQueue []RequestSessionQueue
+}
+
+func NewRequestQueue() *RequestQueue {
+	requestSessionQueue := make([]RequestSessionQueue, 0)
+	return &RequestQueue{
+		requestSessionQueue: requestSessionQueue,
+	}
+}
+
+func (e *RequestQueue) String() string {
+	var builder strings.Builder
+	for _, rq := range e.requestSessionQueue {
+		builder.WriteString(fmt.Sprintf("Request Session Queue ID: %s", rq.sessionID))
+		builder.WriteString(fmt.Sprintf("%s\n", rq.String()))
+	}
+	return fmt.Sprintf("RequestQueue: %s", builder.String())
+}
+
+func (e *RequestQueue) Enqueue(sessionID string, message []byte) {
+	logD(sessionID, "Adding Item To Queue", "RequestQueue.Enqueue")
+	var foundRequestSessionQueue *RequestSessionQueue
+	var rsqIndex = -1
+	for i, rsq := range e.requestSessionQueue {
+		if rsq.sessionID == sessionID {
+			foundRequestSessionQueue = &rsq
+			rsqIndex = i
+			break
+		}
+	}
+	if rsqIndex != -1 {
+		foundRequestSessionQueue.Enqueue(message)
+		e.requestSessionQueue[rsqIndex] = *foundRequestSessionQueue
+	} else {
+		foundRequestSessionQueue = NewRequestSessionQueue(sessionID)
+		foundRequestSessionQueue.Enqueue(message)
+		e.requestSessionQueue = append(e.requestSessionQueue, *foundRequestSessionQueue)
+	}
+	logD(sessionID, e.String(), "RequestQueue.Enqueue")
+}
+
+func (e *RequestQueue) DequeueFirst() RequestSessionQueue {
+	element := e.requestSessionQueue[0] //Get element at index
+	logD(element.sessionID, fmt.Sprintf("Dequeue Item at Position: %d", 0), "RequestQueue.DequeueFirst")
+	e.requestSessionQueue = e.requestSessionQueue[1:] // Slice off the element once it is dequeued.
+	logD(element.sessionID, e.String(), "RequestQueue.Dequeue")
+	return element
+}
+
+func (e *RequestQueue) Dequeue(index int) RequestSessionQueue {
+	element := e.requestSessionQueue[index] //Get element at index
+	logD(element.sessionID, fmt.Sprintf("Dequeue Item at Position: %d", index), "RequestQueue.Dequeue")
+	e.requestSessionQueue = append(e.requestSessionQueue[:index], e.requestSessionQueue[index+1:]...) // Slice off the element once it is dequeued.
+	logD(element.sessionID, e.String(), "RequestQueue.Dequeue")
+	return element
+}
+
+type RequestSessionQueue struct {
+	sessionID       string
+	requestObjQueue []RequestQueueObject
+}
+
+func NewRequestSessionQueue(sessionID string) *RequestSessionQueue {
+	requestObjQueue := make([]RequestQueueObject, 0)
+	return &RequestSessionQueue{
+		requestObjQueue: requestObjQueue,
+		sessionID:       sessionID,
+	}
+}
+
+func (e *RequestSessionQueue) String() string {
+	var builder strings.Builder
+	for _, rq := range e.requestObjQueue {
+		builder.WriteString(fmt.Sprintf("%s\n", rq.String()))
+	}
+	return fmt.Sprintf("RequestSessionQueue: %s", builder.String())
+}
+
+func (e *RequestSessionQueue) Enqueue(message []byte) {
+	logD(e.sessionID, "Adding Item To RequestSessionQueue", "RequestSessionQueue.Enqueue")
+	rqo := RequestQueueObject{message, time.Now()}
+	e.requestObjQueue = append(e.requestObjQueue, rqo)
+	logD(e.sessionID, fmt.Sprintf("Request Session Queue After Adding Object: %s", e.String()), "RequestSessionQueue.Enqueue")
+}
+
+func (e *RequestSessionQueue) DequeueFirst() RequestQueueObject {
+	element := e.requestObjQueue[0] //Get element at index
+	logD(e.sessionID, fmt.Sprintf("Dequeue Item at Position: %d", 0), "RequestSessionQueue.DequeueFirst")
+	e.requestObjQueue = e.requestObjQueue[1:] // Slice off the element once it is dequeued.
+	logD(e.sessionID, fmt.Sprintf("RequestSessionQueue after dequeue: %s", e.String()), "RequestSessionQueue.DequeueFirst")
+	return element
+}
+
 type RequestQueueObject struct {
-	sessionID   string
 	message     []byte
 	requestTime time.Time
 }
 
-type RequestQueue struct {
-	requestObjQueue []RequestQueueObject
-}
-
-func NewRequestQueue() *RequestQueue {
-	requestObjQueue := make([]RequestQueueObject, 0)
-	return &RequestQueue{
-		requestObjQueue: requestObjQueue,
-	}
-}
-
 func (e *RequestQueueObject) String() string {
-    return fmt.Sprintf("RequestQueueObject: %s: %s", e.requestTime.String(), e.message)
+	return fmt.Sprintf("RequestQueueObject: %s: %s", e.requestTime.String(), e.message)
 }
 
-func (e *RequestQueue) String() string {
-    var builder strings.Builder
-    for _, rq := range e.requestObjQueue {
-        builder.WriteString(fmt.Sprintf("%s\n", rq.String()))
-    }
-    return fmt.Sprintf("RequestQueue: %s", builder.String())
-}
-
-// Logical flow from the queue
-func (e *RequestQueue) Work() {
+func (reqQueue *RequestQueue) Work() {
 	for {
-	workerLoop:
-		for i, rq := range e.requestObjQueue {
-			client, ok := ClientMgr.GetClient(rq.sessionID)
-			if ok {
-				logD(rq.sessionID, fmt.Sprintf("Found Client for Queued Object: %s", &rq), "Work")
-				err := client.conn.WriteMessage(1, rq.message)
-				if err == nil {
-					logD(rq.sessionID, "No Error Sending Request, Removing", "Work")
-					if i != 0 {
-						e.Dequeue(i)
-					} else {
-						e.DequeueFirst()
+		for reqSessionQueueIndex, reqSessionQueue := range reqQueue.requestSessionQueue {
+			client, clientFound := ClientMgr.GetClient(reqSessionQueue.sessionID)
+			for _, reqQueueObject := range reqSessionQueue.requestObjQueue {
+				if reqQueueObject.requestTime.Add(time.Second * 30).Before(time.Now()) {
+					logD(reqSessionQueue.sessionID, "Request Is Older Than 30s, Removing", "Work")
+					reqSessionQueue.DequeueFirst()
+					reqQueue.requestSessionQueue[reqSessionQueueIndex] = reqSessionQueue
+					break
+				} else if clientFound {
+					err := client.conn.WriteMessage(1, reqQueueObject.message)
+					if err == nil {
+						logD(reqSessionQueue.sessionID, "No Error Sending Request, Removing", "Work")
+						reqSessionQueue.DequeueFirst()
+						reqQueue.requestSessionQueue[reqSessionQueueIndex] = reqSessionQueue
+						break
 					}
-					break workerLoop
+				} else {
+					break
 				}
-			} else {
-				if rq.requestTime.Add(time.Second * 30).Before(time.Now()) {
-					logD(rq.sessionID, "Request Is Older Than 30s, Removing", "Work")
-					if i != 0 {
-						e.Dequeue(i)
-					} else {
-						e.DequeueFirst()
-					}
-					break workerLoop
+			}
+			if len(reqSessionQueue.requestObjQueue) == 0 {
+				if reqSessionQueueIndex != 0 {
+					reqQueue.Dequeue(reqSessionQueueIndex)
+				} else {
+					reqQueue.DequeueFirst()
 				}
+				break
 			}
 		}
 	}
-}
-
-func (e *RequestQueue) Enqueue(sessionID string, message []byte) {
-	logD(sessionID, "Adding Item To Queue", "Enqueue")
-	rqo := RequestQueueObject{sessionID, message, time.Now()}
-	e.requestObjQueue = append(e.requestObjQueue, rqo)
-	logD(sessionID, e.String(), "Enqueue")
-}
-
-func (e *RequestQueue) DequeueFirst() RequestQueueObject {
-	element := e.requestObjQueue[0] //Get element at index
-	logD(element.sessionID, fmt.Sprintf("Dequeue Item at Position: %d", 0), "Dequeue")
-	e.requestObjQueue = e.requestObjQueue[1:] // Slice off the element once it is dequeued.
-	logD(element.sessionID, e.String(), "Dequeue")
-	return element
-}
-func (e *RequestQueue) Dequeue(index int) RequestQueueObject {
-	element := e.requestObjQueue[index] //Get element at index
-	logD(element.sessionID, fmt.Sprintf("Dequeue Item at Position: %d", index), "Dequeue")
-	e.requestObjQueue = append(e.requestObjQueue[:index], e.requestObjQueue[index+1:]...) // Slice off the element once it is dequeued.
-    logD(element.sessionID, e.String(), "Dequeue")
-	return element
 }
